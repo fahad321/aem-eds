013f601ac8fb26a8c9ba5dbb7c691154f94405cc
Fahad Shafiq <fahad.shafiq@accenture.com>
2024-04-23 15:17:43 +1000

    Fixed identations

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -6,16 +6,16 @@ export default function decorate(block) {
     breadcrumbContainer.setAttribute('aria-label', 'Breadcrumb');
     const ol = document.createElement('ol');
     breadcrumbContainer.appendChild(ol);
-  
+
     pathnames.forEach((path, index) => {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.href = '/' + pathnames.slice(0, index + 1).join('/') + '/';
-      a.textContent = path; // Assuming the path segment can be used as the breadcrumb name
-      li.appendChild(a);
-      ol.appendChild(li);
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = '/' + pathnames.slice(0, index + 1).join('/') + '/';
+        a.textContent = path; // Assuming the path segment can be used as the breadcrumb name
+        li.appendChild(a);
+        ol.appendChild(li);
     });
-  
+
     block.innerHTML = '';
     block.appendChild(breadcrumbContainer);
-  }
+}
