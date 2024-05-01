b4b844d27efccbcb04cf8cc116872b2c3a4c9ede
Fahad Shafiq <fahad.shafiq@accenture.com>
2024-05-01 12:27:57 +1000

    Refactor quote decoration logic and add blockquote element creation with console logs for testing

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -2,6 +2,8 @@ export default function decorate(block) {
   const [quoteWrapper] = block.children;
   // eslint-disable-next-line no-console
   console.log(block);
+  // eslint-disable-next-line no-console
+  console.log(quoteWrapper);
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
